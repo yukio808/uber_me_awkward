19657c9c1e1c74717c7684b7bc11b45172bad3f1
styel07 <eleazarcorpuz@gmail.com>
2015-10-24 20:33:55 -1000

    create db and insert query working

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,7 +3,9 @@
 -- DROP DATABASE IF EXISTS db_uber_me_awkward;
 
 -- -- create the database
--- CREATE DATABASE IF NOT EXISTS db_uber_me_awkward;
+-- CREATE DATABASE db_uber_me_awkward;
+
+
 
 -- request a ride table
 CREATE TABLE request_ride (
@@ -11,28 +13,29 @@ product_id character(36),
 start_latitude double precision,
 start_longitude double precision,
 end_latitude double precision,
-end_longitude double precision
-);
+end_longitude double precision,
 
--- users table
-CREATE TABLE users (
-id serial PRIMARY KEY,
+user_id serial,
 username character varying(24),
 password character varying(12)
 );
 
--- INSERT INTO request_ride (
---   product_id,
---   start_latitude,
---   start_longitude,
---   end_latitude,
---   end_longitude
---   )
--- VALUES();
+INSERT INTO request_ride (
+  product_id,
+  start_latitude,
+  start_longitude,
+  end_latitude,
+  end_longitude,
+  username,
+  password)
+VALUES(
+  '821415d8-3bd5-4e27-9604-194e4359a449',
+  34.232,
+  234.45,
+  48.321,
+  218.32,
+  'uberuser@gmail.com',
+  '1234password'
+  );
 
--- INSERT INTO users (
---   id,
---   username,
---   password
---   )
--- VALUES();
+SELECT * FROM request_ride;
