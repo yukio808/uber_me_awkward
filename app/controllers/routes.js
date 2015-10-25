427f991a8aed1c4bf67b304b3329b2656f985935
yukio yamamoto <aj.yamamoto@me.com>
2015-10-24 18:05:51 -1000

    sucessful connection to api storing local auth token from uber api oAuth

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -1,13 +1,14 @@
 // module/dependancies/env =================================================
 var app               = require('express');
 var router            = app.Router();
+var session           = require('express-session');
 var request           = require('request');
 var OAuth2            = require('oauth').OAuth2;
 var uberApiUrl        = 'https://sandbox-api.uber.com/v1/';
 var uberServerToken   = process.env.UBER_SERVER_TOKEN;
 var uberClientID      = process.env.UBER_CLIENT_ID;
 var uberClientSecret  = process.env.UBER_CLIENT_SECRET;
-var serverUrl         = 'http://' + require("os").hostname() + ':' + ( process.env.PORT || 3000 );
+var serverUrl         = 'http://localhost' + ':' + ( process.env.PORT || 8080 );
 //==========================================================================
 var oauth2 = new OAuth2(
   uberClientID,
@@ -49,28 +50,80 @@ router.get('/estimate/price', function (req, res){
 
 // Post: Request Ride
 //
+router.post('/request_ride', function (req, res){
+  var source = {lat: '21.2969690', lng: '-157.8565750'}; // box jelly
+  var destination = {lat: '21.3607130', lng: '-157.8887950'}; // tripler army med center
+  var product_id = '';
+  console.log(req);
 
+  if( !req.body.hasOwnProperty('auth_token') ){ // handler for surge pricing
+    return res.json({
+      success : false,
+      code : 401,
+      popupUrl : getAuthorizeUrl()
+    });
+  }
 
+  var uberRequest = {
+    start_latitude : source.lat,
+    start_longitude : source.lng,
+    end_latitude : destination.lat,
+    end_longitude : destination.lng,
+    product_id : product_id
+  };
 
+  request.post({
+    url : uberApiUrl + 'requests',
+    json : uberRequest,
+    strictSSL: false,
+    auth : {
+      bearer : req.body.auth_token
+    }
+  }, function(err, response, body){
+    if(err){
+      return res.json(err);
+    }
+    body.success = true;
+    res.json(body);
+  });
 
+});
 
+router.get('/oauth/cb', function(req, res){
+  var code = req.query.code;
+  oauth2.getOAuthAccessToken(
+    code,
+    { // NOT IN THE UBER DOCS
+      grant_type: 'authorization_code',
+      redirect_uri: serverUrl+'/api/oauth/cb'
+    },
+    function (err, access_token, refresh_token, results){
+      if(err){
+        console.log(err);
+        if(err.data){
+          res.end(err.data);
+        }else{
+          res.json(err);
+        }
+      } else if(results.error) {
+        console.log(results.error);
+        res.json(results.error);
+      } else {
+        // got token, send back to client
+        // POPUP Blocker must be disabled, or find workaround, or use redirect instead
+        res.send(closeAndRedirectScript(access_token));
+      }
+    });
+});
+// get request sent 
+//
 ////
 function closeAndRedirectScript(access_token) {
-  return '<script> \
-          if (window.opener != null && !window.opener.closed){ \
-            window.opener.location = "'+redirectAccessTokenUrl(access_token)+'"; \
-            window.close(); \
-          }else{ \
-            document.write("Pop-up blocker prevented proper authorization process. Please disable and re-authorize."); \
-          } \
-          </script>';
-}
-
-function redirectAccessTokenUrl(access_token) {
-  return serverUrl + '#store-auth-token/' + access_token;
+  return "<script> localStorage['auth_token'] = "+ "'"+ access_token + "'" + "; window.location.replace('/api'); </script>";
 }
 
 function getAuthorizeUrl(){
+  console.log('got here');
   return oauth2.getAuthorizeUrl({
     redirect_uri: serverUrl + '/api/oauth/cb',
     scope: ['request'],
